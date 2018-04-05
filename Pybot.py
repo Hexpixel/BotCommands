d4028b314f5362fb0f188d78f4865f248c06f19d
Sean <35079464+Hexpixel@users.noreply.github.com>
2018-04-05 07:36:18 -0400

    Add files via upload

---
+++ Pybot.py
@@ -0,0 +1,25 @@
+import discord
+
+# creates a new Discord client
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    elif message.content.startswith('!bot'):
+        await client.send_message(message.channel, "Yes?")
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run('NDMxMjU5MjYyNzI4MjczOTMw.DacJtA.a4rVp5q8vIISkMzgcH9n7Oi5scE')
