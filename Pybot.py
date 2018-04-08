045a2d27aa9cfa0d3c36ca47c2d50a2fe28ad3e3
Sean <35079464+Hexpixel@users.noreply.github.com>
2018-04-07 20:35:34 -0400

    Update Pybot.py

--- Pybot.py
+++ Pybot.py
@@ -1,7 +1,7 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
 import random
-import sys
 
 # creates a new Discord client
 client = discord.Client()
@@ -10,10 +10,8 @@ token = 'token'
 
 @client.event
 async def on_ready():
-    print('Successfully logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print(client.user.name, client.user.id, "is connected to Discord.")
+    print('------|------|------|------|------|------|------')
 
 @client.event
 async def on_message(message):
@@ -24,14 +22,30 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
-    elif message.content.startswith('!bot'):
+    if message.content.startswith('!bot'):
         await client.send_message(message.channel, "Yes?")
-    elif message.content.startswith('!flip'):
+    if message.content.startswith('!flip'):
         flip = random.choice(['Heads!', 'Tails!'])
         await client.send_message(message.channel, flip)
-    elif message.content.startswith('!color'):
+    if message.content.startswith('!color'):
         color = ['red', 'green', 'blue', 'orange', 'grey', 'purple', 'yellow', 'black', 'gold']
         randomized = random.choice(color)
         await client.send_message(message.channel, randomized)
-        
+    userID = message.author.id
+    if message.content.upper().startswith('!SAY'):
+        #'your ID' = my ID
+        if message.author.id == 'your ID':
+            args = message.content.split(" ")
+            await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+        else:
+            await client.send_message(message.channel, "You are not authorized!")
+            # "your admin role" = Admin role ID
+    if message.content.upper().startswith('!AMIADMIN'):
+        if "your admin role" is (role.id for role in message.author.roles):
+            await client.send_message(message.channel, "You are an admin")
+        else:
+            await client.send_message(message.channel, "You are not an admin")
+
+            # other code for this specific if statement can go here please
+
 client.run(token)
